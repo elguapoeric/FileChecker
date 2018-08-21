f4c1dd635c2ffa7b20fd15c648c65d8b81eaa0ef
Eric <erictinajero@mail.weber.edu>
2018-08-21 14:57:34 -0600

    Add project files.

---
+++ FileChecker/MonitorFiles.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace FileChecker
+{
+    class MonitorFiles
+    {
+        string path = @"C:\Users\Eric\Desktop\myfiles.txt";
+        public MonitorFiles()
+        {
+            
+            
+            //Using the FileSystemWatcher to monitor file changes in a specific file
+            FileSystemWatcher monitor = new FileSystemWatcher(@"C:\Users\Eric\Downloads");
+            monitor.EnableRaisingEvents = true;
+            monitor.IncludeSubdirectories = false; //dont want to monitor subdirectories
+            
+
+
+            //Add event handler
+            monitor.Changed += monitor_Changed;
+            monitor.Created += monitor_Created;
+            monitor.Deleted += monitor_Deleted;
+            monitor.Renamed += monitor_Renamed;
+
+
+
+            Console.Read();
+        }
+
+        private void monitor_Renamed(object sender, RenamedEventArgs e)
+        {
+            if (!File.Exists(path))
+            {
+                string welcome = $"LIST OF FILES DELETED OR MODIFIED" + Environment.NewLine;
+                File.WriteAllText(path, welcome);
+            }
+            
+            string message = $"{e.OldName} has been renamed to {e.Name} at {DateTime.Now.ToLocalTime()}" + Environment.NewLine;
+            File.AppendAllText(path, message);
+            Console.WriteLine(message);
+
+        }
+
+        private void monitor_Deleted(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(path))
+            {
+                string welcome = $"LIST OF FILES DELETED OR MODIFIED" + Environment.NewLine;
+                File.WriteAllText(path, welcome);
+            }
+
+            string message = $"{e.Name} was deleted at {DateTime.Now.ToLocalTime()}" + Environment.NewLine;
+            File.AppendAllText(path, message);
+            Console.WriteLine(message);
+
+        }
+
+        private void monitor_Created(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(path))
+            {
+                string welcome = $"LIST OF FILES DELETED OR MODIFIED" + Environment.NewLine;
+                File.WriteAllText(path, welcome);
+            }
+
+            string message = $"{e.Name} was created at {DateTime.Now.ToLocalTime()}" + Environment.NewLine; ;
+            File.AppendAllText(path, message);
+            Console.WriteLine(message);
+            
+        }
+
+        private void monitor_Changed(object sender, FileSystemEventArgs e)
+        {
+            if (!File.Exists(path))
+            {
+                string welcome = $"LIST OF FILES DELETED OR MODIFIED" + Environment.NewLine;
+                File.WriteAllText(path, welcome);
+            }
+
+            string message = $"{e.Name} was modified at {DateTime.Now.ToLocalTime()}" + Environment.NewLine; ;
+            File.AppendAllText(path, message);
+            Console.WriteLine(message);
+
+        }
+    }
+}
